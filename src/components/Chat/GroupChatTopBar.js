0e05492f05c78a371a5644c7ad23b2ff2991ef7d
muzammil <mynameismuzammilhussainshah@gmail.com>
2022-11-30 22:24:24 +0500

    groupd defaultName isue [FIX]

--- src/components/Chat/GroupChatTopBar.js
+++ src/components/Chat/GroupChatTopBar.js
@@ -14,7 +14,7 @@ export default class GroupChatTopBar extends Component {
             <View style={[this.props.style ?? {}, styles.container]}>
                 <View style={styles.groupNameContainer}>
                     <Text style={styles.groupNameTxt}>
-                        {this.props.name ? this.props.name : `Val d'lsere`}
+                        {this.props.name ? this.props.name : `Group Chat`}
                     </Text>
                     <FlatList
                         data={this.props.member}
